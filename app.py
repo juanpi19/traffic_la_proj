c86acc53ce439a97994b8c7e7fa594c35e2d0dfd
“juanpi19” <juanpablacho19@gmail.com>
2023-11-20 11:25:07 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,251 @@
+import streamlit as st
+import pandas as pd
+import geocoder
+import sqlite3
+from datetime import datetime, timedelta
+import pytz  # for working with time zones
+from pw import bing_map_api_endpoint, parking_meter_occupancy_api_endpoint
+from utility import get_coordinates, calculates_distance_and_driving_time_from_point_a_to_point_b, api_request, \
+                    ingests_parking_meter_live_data_to_parking_meter_occupancy_live_t, haversine, \
+                    fetch_autocomplete_suggestions, joins_street_parking_inventory_with_live_api_data
+
+
+st.set_page_config(layout="wide")
+
+st.title("Your Solution to Street Parking in the City of LA")
+
+col1, col2, col3 = st.columns(3, gap='large')
+
+
+
+if 'current_location' not in st.session_state:
+        st.session_state.current_location = ""
+
+
+from_address_user_input = col1.text_input("From - (if you type and hit enter you'll see the suggestions below) ", value=st.session_state.current_location)
+
+
+if from_address_user_input:
+    suggestions = fetch_autocomplete_suggestions(from_address_user_input)
+    from_address = col1.selectbox("Suggested Places - (From)", options=suggestions)
+
+if st.button('Current Location'):
+    from_address = f" {geocoder.ip('me').latlng[0]},{geocoder.ip('me').latlng[1] } ".strip()
+    st.session_state.current_location = from_address
+
+
+
+to_address_user_input = col2.text_input("To - (if you type and hit enter you'll see the suggestions below)", value="317 S Broadway, Los Angeles, CA 90013")
+
+if to_address_user_input:
+    suggestions = fetch_autocomplete_suggestions(to_address_user_input)
+    to_address = col2.selectbox("Destination", options=suggestions)
+
+
+radius = col3.text_input("Enter Distance in meters you're willing to walk (100 meters = 1 block)", value="")
+
+
+st.write("")
+st.write("")
+
+map_total_parking_inventory, map_available_parking = st.columns(2, gap='large')
+
+
+# Displaying Map
+
+if 'initial_map' not in st.session_state:
+    st.session_state.initial_map = True
+
+if st.session_state.initial_map:
+
+    final_df = joins_street_parking_inventory_with_live_api_data()
+
+
+    map_total_parking_inventory.subheader(f'{final_df.shape[0]} Street Parking Spots with Live Data')
+    #map_total_parking_inventory.write('As soon as you update the widgets above, this map will be refreshed')
+    map_total_parking_inventory.map(final_df[['lat', 'lon']])
+    st.caption('ToDo: get more live parking data from other parts of LA')
+
+###
+
+
+
+
+if st.session_state.current_location != '':
+
+    #st.write('hello')
+
+    #st.session_state.initial_map = False
+
+    if to_address_user_input and radius:
+
+        from_address_coordinates = st.session_state.current_location #from_address #get_coordinates(from_address) 
+        to_address_coordinates = get_coordinates(to_address)
+
+
+        if from_address_coordinates and to_address_coordinates:
+                
+            distance, duration = calculates_distance_and_driving_time_from_point_a_to_point_b(from_address_coordinates, to_address_coordinates)
+
+
+            if distance and duration:
+
+            
+                lat = [float(from_address_coordinates.split(',')[0]), float(to_address_coordinates.split(',')[0])]
+                long = [float(from_address_coordinates.split(',')[1]), float(to_address_coordinates.split(',')[1])]
+
+                df = pd.DataFrame({'lat': lat, 'lon': long, 'type': ['destination', 'destination']})
+
+
+                final_df = joins_street_parking_inventory_with_live_api_data()
+                final_df = final_df[final_df['occupancystate'] == 'VACANT']
+
+
+                destination_lat = float(to_address_coordinates.split(',')[0])
+                destination_lon = float(to_address_coordinates.split(',')[1])
+
+                street_parking_spaceid_in_proximity_list = []
+
+
+                for index, row in final_df.iterrows():
+
+                    if int(radius) >= haversine(destination_lat, destination_lon, float(row['lat']), float(row['long'])):
+
+                        street_parking_spaceid_in_proximity_list.append(row['spaceid'])
+
+                if len(street_parking_spaceid_in_proximity_list) <= 0:
+                    map_available_parking.write("Sorry, we didn't find anything in proximity. Try increasing the radius!")
+                
+                else:
+
+                    final_df = final_df[final_df['spaceid'].isin(street_parking_spaceid_in_proximity_list)]
+                    final_df['type'] = 'parking'
+
+
+                    available_parking_df = pd.concat([final_df[['lat', 'lon', 'type']], df])
+
+                    map_available_parking.subheader(f"{len(street_parking_spaceid_in_proximity_list)} Street Parking Available!")
+
+                    available_parking_df.loc[(available_parking_df['type'] == 'destination'), 'color' ] = "#ff0000"
+                    available_parking_df.loc[(available_parking_df['type'] == 'parking'), 'color' ] = "#0000FF"
+
+
+
+                    map_available_parking.map(data=available_parking_df, latitude='lat', longitude='lon', size='type', color="color")
+                    map_available_parking.write(f"🏁 distance in kilometers: {distance}")
+                    map_available_parking.write(f"🚗 time in minutes: {round(duration)}, get there by {(datetime.now() + timedelta(minutes=duration)).time()}")
+                    map_available_parking.write(available_parking_df)
+
+
+
+else:
+
+   # st.session_state.initial_map = False
+
+    if from_address_user_input and to_address_user_input and radius:
+
+        from_address_coordinates = get_coordinates(from_address) 
+        to_address_coordinates = get_coordinates(to_address)
+
+
+        if from_address_coordinates and to_address_coordinates:
+            
+            distance, duration = calculates_distance_and_driving_time_from_point_a_to_point_b(from_address_coordinates, to_address_coordinates)
+
+
+            if distance and duration:
+                # map_available_parking.write(f"🏁 distance in kilometers {distance}")
+                # map_available_parking.write(f"🚗 time in minutes {round(duration)}")
+
+            
+                lat = [float(from_address_coordinates.split(',')[0]), float(to_address_coordinates.split(',')[0])]
+                long = [float(from_address_coordinates.split(',')[1]), float(to_address_coordinates.split(',')[1])]
+
+                df = pd.DataFrame({'lat': lat, 'lon': long, 'type': ['destination', 'destination']})
+
+
+                final_df = joins_street_parking_inventory_with_live_api_data()
+                final_df = final_df[final_df['occupancystate'] == 'VACANT']
+
+
+                destination_lat = float(to_address_coordinates.split(',')[0])
+                destination_lon = float(to_address_coordinates.split(',')[1])
+
+                street_parking_spaceid_in_proximity_list = []
+
+
+                for index, row in final_df.iterrows():
+
+                    if int(radius) >= haversine(destination_lat, destination_lon, float(row['lat']), float(row['long'])):
+
+                        street_parking_spaceid_in_proximity_list.append(row['spaceid'])
+
+                if len(street_parking_spaceid_in_proximity_list) <= 0:
+                    map_available_parking.write("Sorry, we didn't find anything in proximity. Try increasing the radius!")
+                
+                else:
+
+                    final_df = final_df[final_df['spaceid'].isin(street_parking_spaceid_in_proximity_list)]
+                    final_df['type'] = 'parking'
+
+
+                    available_parking_df = pd.concat([final_df[['lat', 'lon', 'type']], df ])
+
+                    map_available_parking.subheader(f"{len(street_parking_spaceid_in_proximity_list)} in proximity!")
+
+                    available_parking_df.loc[(available_parking_df['type'] == 'destination'), 'color' ] = "#ff0000"
+                    available_parking_df.loc[(available_parking_df['type'] == 'parking'), 'color' ] = "#0000FF"
+
+
+
+                    map_available_parking.map(data=available_parking_df, latitude='lat', longitude='lon', size='type', color="color")
+                    map_available_parking.write(f"🏁 distance in kilometers: {distance}")
+
+                    map_available_parking.write(f"🚗 time in minutes: {round(duration)}, get there by {(datetime.now() + timedelta(minutes=duration)).time()}")
+                    map_available_parking.write(available_parking_df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############
+
+# import sqlite3
+
+# df = api_request(api_endpoint=parking_meter_occupancy_api_endpoint, api_name='Socrata')
+# ingests_parking_meter_live_data_to_parking_meter_occupancy_live_t(df) 
+
+# conn = sqlite3.connect('/Users/juanherrera/Desktop/MacBook Pro/Data Science/Projects/traffic_la_project/traffic.db')
+# cursor = conn.cursor()
+# q = '''SELECT *
+#         FROM metered_parking_inventory  mi
+#         '''
+
+# inventory_df = pd.read_sql_query(q, conn)
+
+# df = pd.merge(df, inventory_df, how='inner', left_on='spaceid', right_on='space_id')
+
+# # df = pd.read_sql_query(q, conn)
+# lat = [float(i) for i in df['lat']]
+# long = [float(i) for i in df['long']]
+# df['lat'] = lat
+# df['long'] = long
+# st.map(df[['lat', 'long']].rename(columns={'lat': 'lat', 'long': 'lon'}))
+# st.write(df.shape)
+# st.write(len(df['space_id'].unique()))
+
+
+
+
+            
+# g = geocoder.ip('me').latlng
+# print(g.latlng)
---
+++ utility.py
@@ -0,0 +1,370 @@
+import sqlite3
+import pandas as pd
+from sodapy import Socrata
+from pw import app_token, username, password, weather_key_api_endpoint, bing_map_api_endpoint, parking_meter_occupancy_api_endpoint
+import requests
+from datetime import datetime
+import pytz  # for working with time zones
+from math import radians, sin, cos, sqrt, atan2
+
+
+
+
+class preprocessing:
+
+    @staticmethod
+    def dict_to_columns_lat_long(dict_):
+        return dict_['latitude'], dict_['longitude'] 
+
+
+def api_request(api_endpoint: str, api_name: str) -> pd.DataFrame:
+    '''
+    This is the function that makes API requests
+
+    Ex: api_request(api_endpoint=parking_meter_inventory_api_endpoint, api_name='Socrata')
+
+    Ex 2: df = api_request(api_endpoint=parking_meter_occupancy_api_endpoint, api_name='Socrata')
+          ingests_parking_meter_live_data_to_parking_meter_occupancy_live_t(df) 
+
+    returns: df - which contains API data
+    '''
+    if api_name.lower().strip() == 'socrata':
+
+        client = Socrata('data.lacity.org',
+                 app_token,
+                 username=username,
+                 password=password)
+        
+        result = client.get(api_endpoint, limit=50000)
+
+        df = pd.DataFrame.from_records(result)
+
+        if api_endpoint == 'e7h6-4a3e':
+            df['eventtime'] = pd.to_datetime(df['eventtime']).dt.tz_localize('UTC')
+
+        return df
+    
+    elif api_name.lower().strip() == 'weather':
+
+        url = 'https://api.openweathermap.org/data/2.5/weather'
+        params = {'APPID': api_endpoint, 'q': 'Los Angeles', 'units': 'celsius'}
+        response = requests.get(url, params=params)
+        weather = response.json()
+        
+        temp = weather['main']['temp'] - 273.15
+        condition = weather['weather'][0]['description']
+        df = pd.DataFrame({'condition': condition, 'temp': temp, 'eventtime_utc': datetime.now(pytz.utc).isoformat()}, index=[1])
+        #df['eventtime_utc'] = pd.to_datetime(df['eventtime'])
+
+        return df #pd.DataFrame({'condition': condition, 'temp': temp}, index=[1])
+
+
+
+
+def ingests_parking_meter_live_data_to_parking_meter_occupancy_live_t(df: pd.DataFrame) -> None:
+    '''
+    This functions takes a dataframe collected from the parking meter live API endpoint data and stores
+    it in the parking_meter_occupancy_live table in the database
+
+    returns: None
+    
+    '''
+    try:
+        conn = sqlite3.connect('/Users/juanherrera/Desktop/traffic.db')
+        cursor = conn.cursor()
+        for _, row in df.iterrows():
+            eventtime_string = row['eventtime'].strftime('%Y-%m-%d %H:%M:%S')
+            space_id = row['spaceid']
+
+            # Check if the record already exists
+            select_query = "SELECT COUNT(*) FROM parking_meter_occupancy_live WHERE space_id = ? AND eventtime_utc = ?"
+            cursor.execute(select_query, (space_id, eventtime_string))
+            count = cursor.fetchone()[0]
+
+            if count == 0:
+                # The record doesn't exist, so insert it
+                insert_query = "INSERT INTO parking_meter_occupancy_live (space_id, eventtime_utc, occupancy_state) VALUES (?, ?, ?);"
+                cursor.execute(insert_query, (space_id, eventtime_string, row['occupancystate']))
+
+            else:
+                pass
+
+        conn.commit()
+
+        #     insert_query = "INSERT INTO parking_meter_occupancy_live (space_id, eventtime_utc, occupancy_state) VALUES (?, ?, ?);"
+        #     cursor.execute(insert_query, (row['spaceid'], eventtime_string, row['occupancystate']))
+        # conn.commit()
+    
+    except:
+        raise Exception("Data didn't make it to database")
+    print('Success')
+
+    
+
+def ingests_parking_meter_inventory_to_metered_parking_inventory_t(df: pd.DataFrame) -> None:
+    '''
+    This functions takes a dataframe collected from the parking meter inventory API endpoint data and stores
+    it in the metered_parking_inventory table in the database
+
+    returns: None
+    
+    '''
+    try:
+        conn = sqlite3.connect('/Users/juanherrera/Desktop/traffic.db')
+        cursor = conn.cursor()
+        for _, row in df.iterrows():
+            insert_query = '''INSERT INTO metered_parking_inventory
+                                        (space_id, block_face, meter_type, rate_type, rate_range, metered_time_limit, lat, long) 
+                                        VALUES (?, ?, ?, ?, ?, ?, ?, ?);'''
+            cursor.execute(insert_query, (row['spaceid'], row['blockface'], row['metertype'], 
+                                          row['ratetype'], row['raterange'], row['timelimit'],
+                                          preprocessing.dict_to_columns_lat_long(row['latlng'])[0],
+                                          preprocessing.dict_to_columns_lat_long(row['latlng'])[1]
+                                          ))
+        conn.commit()
+    
+    except:
+        raise Exception("Data didn't make it to database")
+    print('Success')
+    
+
+def ingests_weather_data_to_weather_t(df: pd.DataFrame) -> None:
+    '''
+    This function takes a dataframe containing the weather data and stores it in the database
+
+    returns: None
+    
+    '''
+    try:
+        conn = sqlite3.connect('/Users/juanherrera/Desktop/traffic.db')
+        cursor = conn.cursor()
+        insert_query = "INSERT INTO weather (eventtime_utc, temperature, condition) VALUES (?, ?, ?);"
+        cursor.execute(insert_query, (datetime.now(pytz.utc).isoformat(), df['temp'].values[0], df['condition'].values[0]))
+        conn.commit()
+    
+    except:
+        raise Exception("Data didn't make it to database")
+    print('Success')
+
+####################
+# Bing Map Functions
+####################
+
+def get_coordinates(place: str) -> str:
+    '''
+    
+    Inputs a place
+    Ex: "USC Marshall School of Business"
+    
+    Ouputs the coodinates as a string
+    Ex: '34.01878357,-118.28292847'
+    
+    '''
+    
+    # Replace with your Bing Maps API key
+    bing_maps_key = bing_map_api_endpoint
+
+    # Set the location to query
+    location = place
+
+    # Construct the URL for the request
+    url = f"https://dev.virtualearth.net/REST/v1/Locations?q={location}&key={bing_maps_key}"
+
+    # Make the request
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+
+        # Extract the latitude and longitude
+        latitude = data["resourceSets"][0]["resources"][0]["point"]["coordinates"][0]
+        longitude = data["resourceSets"][0]["resources"][0]["point"]["coordinates"][1]
+
+        return f' {latitude},{longitude} '.strip()
+    else:
+        raise Exception("Could not parse coordinates")
+
+
+
+def calculates_distance_and_driving_time_from_point_a_to_point_b(from_: str, to: str) -> tuple:
+    
+    '''
+    This function takes two strings that represent the point 1 place and the point 2 place
+    It calculates how long it'll take to drive there and the distance in kilometers
+    
+    Ex: from: (long, lat), to: (long, lat)
+    
+    and returns
+    Ex: (11,234, 27)
+    
+    '''
+    
+    # Replace with your Bing Maps API Key
+    api_key = bing_map_api_endpoint
+
+    # Specify the start and end coordinates (latitude, longitude)
+    start_coordinates = from_ 
+    end_coordinates = to   # Los Angeles, CA
+
+    # Build the URL for the Bing Maps API request
+    base_url = 'https://dev.virtualearth.net/REST/v1/Routes/Driving'
+    params = {
+        'wayPoint.1': start_coordinates,
+        'wayPoint.2': end_coordinates,
+        'key': api_key
+    }
+
+    # Make the API request
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    
+    # # Check for a successful response
+    if response.status_code == 200:
+        # Extract the driving distance and duration (in seconds)
+        if 'resourceSets' in data and len(data['resourceSets']) > 0 and 'resources' in data['resourceSets'][0]:
+            route = data['resourceSets'][0]['resources'][0]
+            driving_distance = route['travelDistance']
+            driving_duration_seconds = route['travelDurationTraffic']  # Includes traffic data
+            driving_duration_minutes = driving_duration_seconds / 60  # Convert to minutes
+            return driving_distance, driving_duration_minutes
+        else:
+            raise Exception("No route information found.")
+    else:
+        raise Exception(f"Error: {response.status_code} - {data['errorDetails'][0]['message']}")
+
+
+
+
+##### Haversine function
+
+def haversine(lat1, lon1, lat2, lon2) -> int:
+    '''
+
+    This function takes two coordinates and calculates the haversine distance in meters
+
+
+    returns: int
+    '''
+
+    # Radius of the Earth in meters
+    R = 6371000.0
+
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    # Differences in coordinates
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    # Haversine formula
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    # Calculate the distance in meters
+    distance = R * c
+
+    return distance
+
+
+
+###############
+
+
+def fetch_autocomplete_suggestions(query, user_location=None, 
+                                   user_circular_map_view=None, 
+                                   user_map_view=None, 
+                                   max_results=10, 
+                                   include_entity_types="Place,Address,Business", 
+                                   culture="en-US", 
+                                   user_region="US", 
+                                   country_filter=None) -> list:
+    
+    '''
+    takes a location (query) calculates the the suggested nearby places using bing's api and returns it as a list
+
+    returns: list
+    
+    '''
+    
+    url = "http://dev.virtualearth.net/REST/v1/Autosuggest"
+
+    params = {
+        "query": query,
+        "key": bing_map_api_endpoint
+    }
+
+    if user_location is not None:
+        params["userLocation"] = user_location
+
+    if user_circular_map_view is not None:
+        params["userCircularMapView"] = user_circular_map_view
+
+    if user_map_view is not None:
+        params["userMapView"] = user_map_view
+
+    params["maxResults"] = max_results
+    params["includeEntityTypes"] = include_entity_types
+    params["culture"] = culture
+    params["userRegion"] = user_region
+
+    if country_filter is not None:
+        params["countryFilter"] = country_filter
+
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        
+        suggestions_list = []
+        
+        
+        for i in response.json()["resourceSets"][0]["resources"][0]['value']:
+            try:
+                suggestions_list.append(i['name'] + ', ' +i['address']['formattedAddress'])
+            except:
+                suggestions_list.append(i['address']['formattedAddress'])
+        
+        
+        return suggestions_list #response.json()#["resourceSets"][0]["resources"][0]#["suggestions"]
+    else:
+        raise Exception("Failed to fetch autocomplete suggestions from Bing Maps API. Error code: {}".format(response.status_code))
+
+
+
+
+def joins_street_parking_inventory_with_live_api_data() -> pd.DataFrame:
+    '''
+    when this function is called, it pings the live paking meter API and joins it with the parking meter inventory
+    it then performs some cleaning and returns everything as a dataframe
+
+    returns: df
+    
+    '''
+    conn = sqlite3.connect('/Users/juanherrera/Desktop/traffic.db')
+    cursor = conn.cursor()
+    q = '''SELECT *
+            FROM metered_parking_inventory  mi
+            '''
+
+    api_df = api_request(api_endpoint=parking_meter_occupancy_api_endpoint, api_name='Socrata')
+    ingests_parking_meter_live_data_to_parking_meter_occupancy_live_t(api_df) 
+
+    inventory_df = pd.read_sql_query(q, conn)
+    final_df = pd.merge(api_df, inventory_df, how='inner', left_on='spaceid', right_on='space_id')
+
+    final_df['lat'] = [float(i) for i in final_df['lat']]
+    final_df['lon'] = [float(i) for i in final_df['long']]
+    return final_df
+
+# import sqlite3
+# conn = sqlite3.connect('/Users/juanherrera/Desktop/MacBook Pro/Data Science/Projects/traffic_la_project/traffic.db')
+# cursor = conn.cursor()
+
+# q = '''SELECT *
+#         FROM parking_meter_occupancy_live pm
+#         INNER JOIN metered_parking_inventory mi
+#         ON pm.space_id = mi.space_id
+#         '''
+
+# print(pd.read_sql_query(q, conn))
